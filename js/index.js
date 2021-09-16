5dc6b526fd75f91222fdfa80ea8fb7ee3ed94e81
Abhinandan-Khurana <75041733+Abhinandan-Khurana@users.noreply.github.com>
2021-09-16 19:48:15 +0530

    wavy text animation update (banner)

--- js/index.js
+++ js/index.js
@@ -1,16 +1,13 @@
+window.addEventListener("scroll", onScrollFixNav);
 
-window.addEventListener('scroll', onScrollFixNav)
-
-function onScrollFixNav(){
-    var navbar = document.getElementById("navbar");
-    if(window.scrollY > 100){
-        navbar.style.background = "#000000f7";
-        navbar.style.height = "5%";
-    }
-    else   {
-
-        navbar.style.background = "transparent";
-        navbar.style.height = "8%";
-    } 
-    console.log(window.scrollY)
+function onScrollFixNav() {
+  var navbar = document.getElementById("navbar");
+  if (window.scrollY > 100) {
+    navbar.style.background = "#000000f7";
+    navbar.style.height = "5%";
+  } else {
+    navbar.style.background = "transparent";
+    navbar.style.height = "8%";
+  }
+  console.log(window.scrollY);
 }
