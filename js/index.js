3618591dd31b4cda15befe18fe923e28db1ef754
sourav-ojha <subhamojha9@gmail.com>
2021-09-19 10:30:20 +0530

    size adjustment- glow-effect nav-pos

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ window.addEventListener("scroll", onScrollFixNav);
 function onScrollFixNav() {
   var navbar = document.getElementById("navbar");
   if (window.scrollY > 100) {
-    navbar.style.background = "#000000f7";
-    navbar.style.height = "5%";
+    navbar.style.background = "#111";
+    navbar.style.height = "6%";
   } else {
     navbar.style.background = "transparent";
     navbar.style.height = "8%";
